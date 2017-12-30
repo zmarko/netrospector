2a22b2fa559e245bb58a4cd9129a5b5d256853be
Marko Zivanovic <marko@netrospector.com>
2017-12-31 00:15:00 +0100

    Initial project skeleton setup

---
+++ src/main/java/com/netrospector/NetrospectorApplication.java
@@ -0,0 +1,12 @@
+package com.netrospector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetrospectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetrospectorApplication.class, args);
+    }
+}
